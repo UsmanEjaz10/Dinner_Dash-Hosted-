6bc0aeba4d52cb0deda6f3726fb073e073ab1069
Usman <alie25708@gmail.com>
2023-08-27 21:21:18 +0500

    Initialized Docker file to be deployed later

--- Order/models.py
+++ Order/models.py
@@ -5,16 +5,17 @@ from Item.models import Item
 
 class Order(models.Model):
     STATUS_CHOICES = [
-        ('ordered', 'Ordered'),
-        ('paid', 'Paid'),
-        ('completed', 'Completed'),
-        ('cancelled', 'Cancelled'),
+        ("ordered", "Ordered"),
+        ("paid", "Paid"),
+        ("completed", "Completed"),
+        ("cancelled", "Cancelled"),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    items = models.ManyToManyField(Item, through='OrderItem')
+    items = models.ManyToManyField(Item, through="OrderItem")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     total = models.BigIntegerField(default=0)
+
     def __str__(self):
         return f"Order {self.pk} ({self.user.username}) - {self.status}   {self.created_at}"
 
